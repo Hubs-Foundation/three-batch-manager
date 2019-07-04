ee0a4c119daae18c90617c36c591f7f5e05e60bb
netpro2k <netpro2k@gmail.com>
2019-07-03 18:38:45 -0700

    Remove hubs specific code

--- src/UnlitBatchShader.ts
+++ src/UnlitBatchShader.ts
@@ -32,29 +32,19 @@ export class BatchRawUniformGroup extends RawUniformsGroup {
   uvTransforms: Float32Array;
   mapSettings: Float32Array;
 
-  hubs_interactorOnePos: Float32Array;
-  hubs_interactorTwoPos: Float32Array;
-  hubs_sweepParams: Float32Array;
-
-  hubs_isFrozen: Uint32Array;
-  hubs_time: Float32Array;
-
   private nextIdx: InstanceID;
   private freed: InstanceID[];
   private meshes: BatchableMesh[];
 
   data: ArrayBuffer;
+  offset: number;
 
-  constructor(maxInstances: number, name = "InstanceData") {
-    const hubsDataSize =
-      maxInstances * 4 * 4 + // sweepParams
-      4 *
-        (3 + // interactorOnePos
-        1 + // isFrozen
-        3 + // interactorTwoPos
-        1  // time
-         );
-    super(new ArrayBuffer(maxInstances * INSTANCE_DATA_BYTE_LENGTH + hubsDataSize));
+  constructor(
+    maxInstances: number,
+    name = "InstanceData",
+    data = new ArrayBuffer(maxInstances * INSTANCE_DATA_BYTE_LENGTH)
+  ) {
+    super(data);
     this.setName(name);
 
     let offset = 0;
@@ -72,20 +62,7 @@ export class BatchRawUniformGroup extends RawUniformsGroup {
     this.mapSettings = new Float32Array(this.data, offset, 4 * maxInstances);
     offset += this.mapSettings.byteLength;
 
-    this.hubs_sweepParams = new Float32Array(this.data, offset, 4 * maxInstances);
-    offset += this.hubs_sweepParams.byteLength;
-
-    this.hubs_interactorOnePos = new Float32Array(this.data, offset, 3);
-    offset += this.hubs_interactorOnePos.byteLength;
-
-    this.hubs_isFrozen = new Uint32Array(this.data, offset, 1);
-    offset += this.hubs_isFrozen.byteLength;
-
-    this.hubs_interactorTwoPos = new Float32Array(this.data, offset, 3);
-    offset += this.hubs_interactorTwoPos.byteLength;
-
-    this.hubs_time = new Float32Array(this.data, offset, 1);
-    offset += this.hubs_time.byteLength;
+    this.offset = offset;
 
     this.nextIdx = 0;
     this.freed = [];
@@ -185,25 +162,13 @@ layout(std140) uniform InstanceData {
   vec4 colors[MAX_INSTANCES];
   vec4 uvTransforms[MAX_INSTANCES];
   vec4 mapSettings[MAX_INSTANCES];
-
-  vec4 hubs_SweepParams[MAX_INSTANCES];
-
-  vec3 hubs_InteractorOnePos;
-  bool hubs_IsFrozen;
-
-  vec3 hubs_InteractorTwoPos;
-  float hubs_Time;
-
 } instanceData;
-
-out vec3 hubs_WorldPosition;
 
 in vec3 position;
 in vec2 uv;
 
 #ifdef PSEUDO_INSTANCING
 in float instance;
-flat out uint vInstance;
 #endif
 
 #ifdef VERTEX_COLORS
@@ -234,9 +199,6 @@ void main() {
   vMapSettings = instanceData.mapSettings[instanceIndex];
 
   gl_Position = projectionMatrix * viewMatrix * instanceData.transforms[instanceIndex] * vec4(position, 1.0);
-
-  hubs_WorldPosition = (instanceData.transforms[instanceIndex] * vec4(position, 1.0)).xyz;
-  vInstance = instanceIndex;
 }
 `;
 
@@ -244,28 +206,6 @@ export const fragmentShader = glsl`#version 300 es
 precision highp float;
 precision highp int;
 precision highp sampler2DArray;
-
-
-layout(std140) uniform InstanceData {
-  mat4 transforms[MAX_INSTANCES];
-  vec4 colors[MAX_INSTANCES];
-  vec4 uvTransforms[MAX_INSTANCES];
-  vec4 mapSettings[MAX_INSTANCES];
-
-  vec4 hubs_SweepParams[MAX_INSTANCES];
-
-  vec3 hubs_InteractorOnePos;
-  bool hubs_IsFrozen;
-
-  vec3 hubs_InteractorTwoPos;
-  float hubs_Time;
-
-} instanceData;
-in vec3 hubs_WorldPosition;
-#ifdef PSEUDO_INSTANCING
-flat in uint vInstance;
-#endif
-
 
 uniform sampler2DArray map;
 
@@ -304,49 +244,5 @@ void main() {
 
   int mapIdx = int(vMapSettings.x);
   outColor = texture(map, vec3(uv, mapIdx)) * vColor;
-
-  bool hubs_HighlightInteractorOne = instanceData.hubs_SweepParams[vInstance].z > 0.0;
-  bool hubs_HighlightInteractorTwo = instanceData.hubs_SweepParams[vInstance].w > 0.0;
-  bool hubs_IsFrozen = instanceData.hubs_IsFrozen;
-
-  if (hubs_HighlightInteractorOne || hubs_HighlightInteractorTwo || hubs_IsFrozen) {
-
-      bool hubs_EnableSweepingEffect = true;
-      vec2 hubs_SweepParams = instanceData.hubs_SweepParams[vInstance].xy;
-      float hubs_Time = instanceData.hubs_Time;
-
-      float ratio = 0.0;
-
-      if (hubs_EnableSweepingEffect) {
-          float size = hubs_SweepParams.t - hubs_SweepParams.s;
-          float line = mod(hubs_Time / 500.0 * size, size * 3.0) + hubs_SweepParams.s - size / 3.0;
-
-          if (hubs_WorldPosition.y < line) {
-              // Highlight with a sweeping gradient.
-              ratio = max(0.0, 1.0 - (line - hubs_WorldPosition.y) / (size * 1.5));
-          }
-      }
-
-      // Highlight with a gradient falling off with distance.
-      float pulse = 9.0 + 3.0 * (sin(hubs_Time / 1000.0) + 1.0);
-
-      if (hubs_HighlightInteractorOne) {
-          float dist1 = distance(hubs_WorldPosition, instanceData.hubs_InteractorOnePos);
-          ratio += -min(1.0, pow(dist1 * pulse, 3.0)) + 1.0;
-      }
-
-      if (hubs_HighlightInteractorTwo) {
-          float dist2 = distance(hubs_WorldPosition, instanceData.hubs_InteractorTwoPos);
-          ratio += -min(1.0, pow(dist2 * pulse, 3.0)) + 1.0;
-      }
-
-      ratio = min(1.0, ratio);
-
-      // Gamma corrected highlight color
-      vec3 highlightColor = vec3(0.184, 0.499, 0.933);
-
-      outColor = vec4((outColor.rgb * (1.0 - ratio)) + (highlightColor * ratio), outColor.a);
-  }
-
 }
 `;
